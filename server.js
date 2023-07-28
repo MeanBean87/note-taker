f38cb3de2e1cab198b445ac578cc38c31f0de81f
Michael Mattingly <mikeamatt1@hotmail.com>
2023-07-28 00:10:41 -0400

    Added .use declarations for express.

--- server.js
+++ server.js
@@ -2,3 +2,10 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/assets", express.static(path.join(__dirname, "assets")));
