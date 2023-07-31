5153fd880fae31b44b2585e00ebb7c1488b81529
Michael Mattingly <mikeamatt1@hotmail.com>
2023-07-31 12:37:04 -0400

    Removed old server.js structure.

--- app/routes/notesRouter.js
+++ app/routes/notesRouter.js
@@ -3,14 +3,18 @@ const router = express.Router();
 const path = require("path");
 const notesController = require("../controllers/notesController");
 
+//get route for /notes
 router.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
+//get route for /api/notes
 router.get("/api/notes", notesController.getNotes);
 
+//post route for /api/notes
 router.post("/api/notes", notesController.addNote);
 
+//delete route for /api/notes/:id
 router.delete("/api/notes/:id", notesController.deleteNote);
 
 module.exports = router;
